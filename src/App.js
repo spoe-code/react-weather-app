e1fdbd25a84000557a9a64ad41a8f0a0acfe5d10
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-07-30 17:29:59 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by Sam Poe and is{" "}
-        <a
-          href="https://github.com/spoe-code/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          {" "}
+          This project was coded by Sam Poe and is{" "}
+          <a
+            href="https://github.com/spoe-code/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
