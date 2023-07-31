c42ce4ab0518daaa00ae607c38ff581b31e23018
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-07-31 08:38:40 -0400

    added "noreferrer" to App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
           {" "}
           This project was coded by Sam Poe and is{" "}
           <a
+            rel="noreferer"
             href="https://github.com/spoe-code/react-weather-app"
             target="_blank"
           >
