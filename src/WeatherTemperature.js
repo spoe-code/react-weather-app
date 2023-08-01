ceeb2fca1e6ad90add14559970bf2a189cb97419
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-07-31 23:43:41 -0400

    fixed errors

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -29,10 +29,9 @@ export default function WeatherTemperature(props) {
       </div>
     );
   } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="temperature">{Math.round(fahrenheit())}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             °C
